09d54e70f23fa76002991bd4303cf03547631f83
qizhengwei <***>
2019-12-04 21:06:17 +0800

    init my blogger

---
+++ src/main/java/com/blogger/bloggerService/BloggerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blogger.bloggerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogger/bloggerService/BloggerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogger.bloggerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
